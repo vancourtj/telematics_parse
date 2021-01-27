e0df63fd544f1da0e3ee2cfb53281ee728f86f55
vancourtj <jonathanvancourt0@gmail.com>
2021-01-27 17:46:38 -0500

    change

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -4,46 +4,46 @@ require "./models/trip"
 describe 'Driver' do
     it 'should have no trips until a trip is added' do
         driver = Driver.new('Dan')
-        
+
         expect(driver.trips).to match_array([])
 	
-	end
+    end
 	
     describe '.add_trip' do
         it 'adds a new trip to the list of trips for the Driver class' do
             driver = Driver.new('Dan')
             trip = Trip.new('Dan','07:15','07:45','17.3')
-            
+
             driver.add_trip(trip)
-            
+
             expect(driver.trips.length()).to eq(1)
-        
+
         end
-        
+
         it 'only adds trips for the specific driver' do
             driver = Driver.new('Dan')
             trip1 = Trip.new('Dan','07:15','07:45','17.3')
             trip2 = Trip.new('Bill','07:15','07:45','17.3')
-            
+
             driver.add_trip(trip1)
             driver.add_trip(trip2)
-            
+        
             expect(driver.trips.length()).to eq(1)
-        
+
         end
-        
+
         it 'only adds trips that are within the expected speed range' do
             driver = Driver.new('Dan')
             trip1 = Trip.new('Dan','07:15','07:45','17.3')
             trip2 = Trip.new('Dan','07:00','08:00','100.01') #above 100
             trip3 = Trip.new('Dan','07:00','08:00','4.99') #below 5
-            
+
             driver.add_trip(trip1)
             driver.add_trip(trip2)
             driver.add_trip(trip3)
-            
+
             expect(driver.trips.length()).to eq(1)
-        
+
         end
     end
 	
