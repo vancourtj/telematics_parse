9687165669257c4eb6200d3a3e1b70a12ad611c3
vancourtj <jonathanvancourt0@gmail.com>
2021-01-27 17:51:12 -0500

    format change

--- spec/services/services_spec.rb
+++ spec/services/services_spec.rb
@@ -5,43 +5,43 @@ require "./models/trip"
 describe "line_parse" do
     it 'returns a new instance of the Driver class when the first line segment is "Driver"' do
         type, driver = line_parse(['Driver', 'Dan'])
-        
+
         expect(type).to eq('Driver')
         expect(driver.driver_name).to eq('Dan')
-    
+
     end
-    
+
     it 'returns a new instance of the Trip class when the first line segment is "Trip"' do
         type, trip = line_parse(['Trip','Dan','07:15','07:45','17.3'])
-        
+
         expect(type).to eq('Trip')
         expect(trip.driver_name).to eq('Dan')
         expect(trip.distance).to be_within(0.001).of(17.3)
-    
+
     end
-    
+
     it 'returns nil when not passed a driver or trip' do
         type, driver = line_parse(['nope','driver'])
-        
+
         expect(type).to be_nil
         expect(driver).to be_nil
-    
+
     end
 end
 
 describe 'class_initialization' do
     it 'returns arrays of Driver and Trips class objects when given text input' do
         input = "Driver Dan\nTrip Dan 07:15 07:45 17.3"
-        
+
         driver_list, trip_list = class_initialization(input)
-        
+
         driver = driver_list[0]
         trip = trip_list[0]
-        
+
         expect(driver.driver_name).to eq('Dan')
         expect(trip.driver_name).to eq('Dan')
         expect(trip.distance).to be_within(0.001).of(17.3)
-    
+
     end
 end
 
@@ -52,15 +52,15 @@ describe 'trip_addition' do
         trip1 = Trip.new('Dan','07:15','07:45','17.3')
         trip2 = Trip.new('Bill','01:05','03:32','31.0')
         trip3 = Trip.new('Dan','013:47','16:07','67.5')
-        
+
         driver_list = [driver1,driver2]
         trip_list = [trip1,trip2,trip3]
-        
+
         trip_addition(driver_list,trip_list)
-        
+
         expect(driver1.trips.length()).to eq(2)
         expect(driver2.trips.length()).to eq(1)
-    
+
     end
 end
 
@@ -72,20 +72,20 @@ describe 'driver_sort' do
         trip1 = Trip.new('Dan','07:15','07:45','17.3')
         trip2 = Trip.new('Bill','01:05','03:32','31.0')
         trip3 = Trip.new('Sam','013:47','16:07','67.5')
-        
+
         driver_list = [driver1,driver2,driver3]
         trip_list = [trip1,trip2,trip3]
-        
+
         trip_addition(driver_list,trip_list)
-        
+
         driver_sort(driver_list)
-        
+
         expect(driver_list[0].driver_name).to eq('Sam')
         expect(driver_list[1].driver_name).to eq('Bill')
         expect(driver_list[2].driver_name).to eq('Dan')
-    
+
     end
-    
+
     it 'sorts the driver data alphabetically when distance is tied' do
         driver1 = Driver.new('Dan')
         driver2 = Driver.new('Bill')
@@ -93,17 +93,17 @@ describe 'driver_sort' do
         trip1 = Trip.new('Dan','07:15','07:45','17.3')
         trip2 = Trip.new('Bill','01:05','03:32','31.0')
         trip3 = Trip.new('Sam','013:47','16:07','31.0')
-        
+
         driver_list = [driver1,driver2,driver3]
         trip_list = [trip1,trip2,trip3]
-        
+
         trip_addition(driver_list,trip_list)
-        
+
         driver_sort(driver_list)
-        
+
         expect(driver_list[0].driver_name).to eq('Bill')
         expect(driver_list[1].driver_name).to eq('Sam')
         expect(driver_list[2].driver_name).to eq('Dan')
-    
+
     end
 end
